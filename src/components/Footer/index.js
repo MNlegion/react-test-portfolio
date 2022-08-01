0c7c47d8206186ee8de5d6a8c3efbef686b082b3
MNlegion <rkreuser30@gmail.com>
2022-08-01 15:47:53 -0500

    initial setup for test project

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <div>
+            <a href="https://github.com/MNlegion"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/robert-kreuser-b09bb7133/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+            <a href="https://twitter.com/xRobthewallx"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" className="icon"/></a>
+            <p>
+            &copy; MNlegion  |  <a href="https://drive.google.com/file/d/1nMO2WS2HilQjFtDAM6lo3BInb906QicR/view?usp=sharing" className="link">Download Resume</a>  |  <a href="https://coolors.co/dcedff-94b0da-8f91a2-505a5b-343f3e">Color Palette</a>  |  Made With <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+            </p>
+    
+        </div>
+        )
+    }
+
+export default Footer;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    
+                </li>
+            </ul>
+
+        </div>
+    );
+}
+
+export default Navigation;
